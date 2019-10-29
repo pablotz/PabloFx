eee22f1a5a30bfe2be137d0360d28b2314825639
Pablo Hernández Castillo <pablotroll789@hotmail.com>
2019-10-29 06:32:53 -0600

    Proyecto primeros esquemas

---
+++ src/org/glassware/gui/WindowMain.java
@@ -0,0 +1,56 @@
+package org.glassware.gui;
+
+
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
+import com.jfoenix.transitions.hamburger.HamburgerNextArrowBasicTransition;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class WindowMain extends Application {
+
+    FXMLLoader fxmll;
+    Stage window;
+    Scene scene;
+    
+    
+
+
+    public WindowMain() {
+        fxmll = new FXMLLoader(System.class.getResource("/org/glassware/fxml/window_main_1.fxml"));
+        fxmll.setController(this);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        fxmll.load();
+
+        scene = new Scene(fxmll.getRoot());
+
+        window = primaryStage;
+
+        window.setScene(scene);
+
+        window.show();
+        
+        
+
+    }
+
+ 
+
+
+
+}
+
