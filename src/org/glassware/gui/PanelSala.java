140b68f2b000b3127e40098c2cc0ae4029b695ac
Pablo Hernández Castillo <pablotroll789@hotmail.com>
2019-11-25 07:47:24 -0600

    Pablo - MASTER

--- src/org/glassware/gui/PanelSala.java
+++ src/org/glassware/gui/PanelSala.java
@@ -1,6 +1,8 @@
 package org.glassware.gui;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -14,6 +16,39 @@ public class PanelSala {
     @FXML
     JFXButton btnCerrarModulo;
     WindowMain app;
+    
+        @FXML
+    private JFXTextField txtBuscarSala;
+
+    @FXML
+    private JFXButton btnBuscarSala;
+
+    @FXML
+    private JFXButton btnGuardarSala;
+
+    @FXML
+    private JFXButton btnEliminarSala;
+
+    @FXML
+    private JFXButton btnModificarSala;
+
+    @FXML
+    private JFXButton btnRegistrarSala;
+
+    @FXML
+    private JFXButton btnSelectFotoSala;
+
+    @FXML
+    private JFXTextField txtNombreSala;
+
+    @FXML
+    private JFXTextArea txtDescripcionSala;
+
+    @FXML
+    private JFXTextField txtEstatusSala;
+
+    @FXML
+    private JFXTextField txtSucursalSala;
 
     FXMLLoader fxmll;
 
@@ -34,5 +69,64 @@ public class PanelSala {
     public Node getRoot() {
         return fxmll.getRoot();
     }
+
+    public JFXButton getBtnCerrarModulo() {
+        return btnCerrarModulo;
+    }
+
+    public WindowMain getApp() {
+        return app;
+    }
+
+    public JFXTextField getTxtBuscarSala() {
+        return txtBuscarSala;
+    }
+
+    public JFXButton getBtnBuscarSala() {
+        return btnBuscarSala;
+    }
+
+    public JFXButton getBtnGuardarSala() {
+        return btnGuardarSala;
+    }
+
+    public JFXButton getBtnEliminarSala() {
+        return btnEliminarSala;
+    }
+
+    public JFXButton getBtnModificarSala() {
+        return btnModificarSala;
+    }
+
+    public JFXButton getBtnRegistrarSala() {
+        return btnRegistrarSala;
+    }
+
+    public JFXButton getBtnSelectFotoSala() {
+        return btnSelectFotoSala;
+    }
+
+    public JFXTextField getTxtNombreSala() {
+        return txtNombreSala;
+    }
+
+    public JFXTextArea getTxtDescripcionSala() {
+        return txtDescripcionSala;
+    }
+
+    public JFXTextField getTxtEstatusSala() {
+        return txtEstatusSala;
+    }
+
+    public JFXTextField getTxtSucursalSala() {
+        return txtSucursalSala;
+    }
+
+    public FXMLLoader getFxmll() {
+        return fxmll;
+    }
+    
+    
+    
 
 }
--- src/org/glassware/gui/PanelSucursal.java
+++ src/org/glassware/gui/PanelSucursal.java
@@ -6,6 +6,7 @@
 package org.glassware.gui;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -24,6 +25,36 @@ public class PanelSucursal {
     @FXML
     AnchorPane rootPane;
 
+    @FXML
+    private JFXTextField txtBuscarSucursal;
+
+    @FXML
+    private JFXButton btnBuscarSucursal;
+
+    @FXML
+    private JFXButton btnRegistrarSucursal;
+
+    @FXML
+    private JFXButton btnModificarSucursal;
+
+    @FXML
+    private JFXButton btnGuardarSucursal;
+
+    @FXML
+    private JFXButton btnEliminarSucursal;
+
+    @FXML
+    private JFXTextField txtNombreSucursal;
+
+    @FXML
+    private JFXTextField txtLatitudSucursal;
+
+    @FXML
+    private JFXTextField txtLongitudSucursal;
+
+    @FXML
+    private JFXTextField txtDomicilioSucursal;
+
     FXMLLoader fxmll;
 
     public PanelSucursal(WindowMain app) {
@@ -34,7 +65,7 @@ public class PanelSucursal {
         fxmll = new FXMLLoader(System.class.getResource("/org/glassware/gui/fxml/panel_sucursal.fxml"));
         fxmll.setController(this);
         fxmll.load();
-        
+
         btnCerrarModulo.setOnAction(evt -> {
             app.cerrarModulo();
         });
@@ -43,9 +74,69 @@ public class PanelSucursal {
     public Node getRoot() {
         return fxmll.getRoot();
     }
-    
-    public AnchorPane getRooPane(){
+
+    public AnchorPane getRooPane() {
         return rootPane;
     }
+
+    public JFXButton getBtnCerrarModulo() {
+        return btnCerrarModulo;
+    }
+
+    public WindowMain getApp() {
+        return app;
+    }
+
+    public AnchorPane getRootPane() {
+        return rootPane;
+    }
+
+    public JFXTextField getTxtBuscarSucursal() {
+        return txtBuscarSucursal;
+    }
+
+    public JFXButton getBtnBuscarSucursal() {
+        return btnBuscarSucursal;
+    }
+
+    public JFXButton getBtnRegistrarSucursal() {
+        return btnRegistrarSucursal;
+    }
+
+    public JFXButton getBtnModificarSucursal() {
+        return btnModificarSucursal;
+    }
+
+    public JFXButton getBtnGuardarSucursal() {
+        return btnGuardarSucursal;
+    }
+
+    public JFXButton getBtnEliminarSucursal() {
+        return btnEliminarSucursal;
+    }
+
+    public JFXTextField getTxtNombreSucursal() {
+        return txtNombreSucursal;
+    }
+
+    public JFXTextField getTxtLatitudSucursal() {
+        return txtLatitudSucursal;
+    }
+
+    public JFXTextField getTxtLongitudSucursal() {
+        return txtLongitudSucursal;
+    }
+
+    public JFXTextField getTxtDomicilioSucursal() {
+        return txtDomicilioSucursal;
+    }
+
+    public FXMLLoader getFxmll() {
+        return fxmll;
+    }
+
+    
+    
+    
 
 }
